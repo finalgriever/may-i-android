bf15c4f04cc1a3fd3076c87433d27a3d5ccad927
Jamie Butler <jamie.b@iinet.net.au>
2019-08-20 10:57:01 +0800

    Validating player name input

--- app/src/main/java/com/finalgriever/mayi/MainActivity.kt
+++ app/src/main/java/com/finalgriever/mayi/MainActivity.kt
@@ -5,18 +5,34 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var playerOneName : EditText? = null
+    var playerTwoName : EditText? = null
+    var playerThreeName : EditText? = null
+    var playerFourName : EditText? = null
+    var playerFiveName : EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        playerOneName = findViewById(R.id.playerOneNameText)
+        playerTwoName = findViewById(R.id.playerTwoNameText)
+        playerThreeName = findViewById(R.id.playerThreeNameText)
+        playerFourName = findViewById(R.id.playerFourNameText)
+        playerFiveName = findViewById(R.id.playerFiveNameText)
 
         startGameButton.setOnClickListener {
+            val errors = validatePlayerNames()
+            if (errors.size > 0) {
+
+            }
             startActivity(Intent(this@MainActivity, GameActivity::class.java))
         }
     }
@@ -35,5 +51,17 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun validatePlayerNames(): List<String> {
+        val errors = mutableListOf<String>()
+        var playerOneNameString = playerOneName?.text.toString()
+        var playerTwoNameString = playerTwoName?.text.toString()
+
+        if (playerOneNameString === "" && playerTwoNameString === "") {
+            errors.add("There must be at least two players")
+        }
+
+        return errors
     }
 }
